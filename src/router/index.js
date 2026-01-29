e617c1c81a65092b1771309e074bfb4f8247aa63
joezhusx <15551626+joezhusx@user.noreply.gitee.com>
2026-01-29 08:39:42 +0800

    Switch to hash mode router for static hosting

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useUserStore } from '@/stores/user'
 
 const routes = [
@@ -67,7 +67,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
